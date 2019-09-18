d4da14dc8f51c4f7851cab4f03cfd11e253f1277
zach romer <romerzs14@gmail.com>
2019-09-18 10:37:03 -0700

    Cleanup

--- src/chatter.py
+++ src/chatter.py
@@ -27,20 +27,24 @@ class ChatterApi(Resource):
 
         message_attrs = parse_request(SlackMessageSchema, request)
 
+        token = self.get_token()
+        slack_client = self.setup_slack_client(token)
+        response = slack_client.chat_postMessage(**message_attrs)
+        LOGGER.info(f'Slack client response: {response}')
+        return make_response(jsonify(code=response.status_code, data=response.data), response.status_code)
+
+    @staticmethod
+    def get_token() -> str:
         if os.getenv('AWS_EXECUTION_ENV', None) or os.getenv('CI', None):
             token = os.getenv('SLACK_TOKEN')
         else:
             project_dir = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
             with open(os.path.join(project_dir, 'chatterbox-remote-env.json'), 'r') as env:
                 token = json.load(env)['SLACK_TOKEN']
-        slack_client = self.setup_slack_client()
-        response = slack_client.chat_postMessage(**message_attrs)
-        LOGGER.info(f'Slack client response: {response}')
-        return make_response(jsonify(code=response.status_code, data=response.data), response.status_code)
+        return token
 
     @staticmethod
-    def setup_slack_client():
-        token = os.getenv('SLACK_TOKEN')
+    def setup_slack_client(token: str):
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
