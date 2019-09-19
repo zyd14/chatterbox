3c796be16852638798b48e1b84de9ef4f82e507d
zyd14 <romerzs14@gmail.com>
2019-09-18 22:37:47 -0700

    Doing a bit of reoganization

--- src/setup_app.py
+++ src/setup_app.py
@@ -8,8 +8,10 @@ def setup_app_singleton():
     global app
     global api
     if not app or not api:
+        from src.utils import create_logger
         app = Flask('compile-trigger')
         api = Api(app)
+        app.logger = create_logger(slack=True)
         return app, api
     else:
         return app, api
